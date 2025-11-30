dad29ebb9111b4bd7ff74d98659921226de1bd30
brigh <brigh@wongyitcheng>
2025-11-30 23:20:37 +0800

    hi

---
+++ assignment/src/main/java/com/mycompany/assignment/Assignment.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment;
+
+/**
+ *
+ * @author brigh
+ */
+public class Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("nihao");
+    }
+}
